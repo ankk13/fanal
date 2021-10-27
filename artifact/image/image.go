zhangwenlong8911/trivy

feat(image): revert size (#261)

artifact/image/image.go
@@ -206,7 +206,7 @@ func (a Artifact) inspectLayer(ctx context.Context, diffID string) (types.BlobIn
 	result := new(analyzer.AnalysisResult)
 	limit := semaphore.NewWeighted(parallel)
 
-	opqDirs, whFiles, layerSize, err := walker.WalkLayerTar(r, func(filePath string, info os.FileInfo, opener analyzer.Opener) error {
+	opqDirs, whFiles, err := walker.WalkLayerTar(r, func(filePath string, info os.FileInfo, opener analyzer.Opener) error {
 		if err = a.analyzer.AnalyzeFile(ctx, &wg, limit, result, "", filePath, info, opener); err != nil {
 			return xerrors.Errorf("failed to analyze %s: %w", filePath, err)
 		}
@@ -231,7 +231,6 @@ func (a Artifact) inspectLayer(ctx context.Context, diffID string) (types.BlobIn
 		Applications:  result.Applications,
 		OpaqueDirs:    opqDirs,
 		WhiteoutFiles: whFiles,
-		Size:          layerSize,
 	}
 
 	// Call hooks to modify blob info

ankk13/fanal

artifact/image/image.go
@@ -251,7 +251,7 @@ func (a Artifact) inspectLayer(ctx context.Context, diffID string) (map[types.Ca
 		resultMap[cache.Type()] = new(analyzer.AnalysisResult)
 	}
 	limit := semaphore.NewWeighted(parallel)
-	opqDirs, whFiles, layerSize, err := walker.WalkLayerTar(cr, func(filePath string, info os.FileInfo, opener analyzer.Opener) error {
+	opqDirs, whFiles, err := walker.WalkLayerTar(cr, func(filePath string, info os.FileInfo, opener analyzer.Opener) error {
 		if err = a.analyzer.AnalyzeFile(ctx, &wg, limit, resultMap, filePath, info, opener); err != nil {
 			return xerrors.Errorf("failed to analyze %s: %w", filePath, err)
 		}
@@ -277,7 +277,6 @@ func (a Artifact) inspectLayer(ctx context.Context, diffID string) (map[types.Ca
 			SchemaVersion:     types.BlobJSONSchemaVersion,
 			Digest:            layerDigest,
 			DiffID:            diffID,
-			Size:              int(layerSize),
 			Misconfigurations: misconfs,
 			OpaqueDirs:        opqDirs,
 			WhiteoutFiles:     whFiles,